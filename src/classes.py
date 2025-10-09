5fadca278414995aacfd3ac4f32541c7444ad772
lengvin <zve565ro@gmail.com>
2025-10-10 01:23:53 +0700

    add classes Product and Category

---
+++ src/classes.py
@@ -0,0 +1,13 @@
+class Product:
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
