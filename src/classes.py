4b09cc57585fe7a792e8edf9d8879a0e4c901046
lengvin <zve565ro@gmail.com>
2025-12-09 23:01:51 +0700

    add methods in class Category

--- src/classes.py
+++ src/classes.py
@@ -10,9 +10,20 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.product_count += len(products)
         Category.category_count += 1
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        result = ''
+        for product in self.__products:
+            result = result + f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт. \n'
+        return result
 
 
 class Product:
