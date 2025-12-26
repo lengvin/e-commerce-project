ac3c618248283307a025752da6d7841af9ec276f
lengvin <zve565ro@gmail.com>
2025-12-26 16:14:31 +0700

    add new subclasses Smartphone and LawGrass

--- src/classes.py
+++ src/classes.py
@@ -94,3 +94,37 @@ class Product:
                         print('Некорректный ввод, повторите попытку')
             else:
                 self.__price = new_price
+
+
+class Smartphone(Product):
+    name: str
+    description: str
+    price: float
+    quantity: int
+    efficiency: float
+    model: str
+    memory: int
+    color: str
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    name: str
+    description: str
+    price: float
+    quantity: int
+    country: str
+    germination_period: str
+    color: str
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
